bdb3979b85d62b75727be6728ec69d8685596e7e
apo <tagliabue.jacopo@gmail.com>
2022-07-13 17:34:59 +0200

    add some notes to README

--- README.md
+++ README.md
@@ -13,5 +13,15 @@ Playground for using large language models into the Modern Data Stack for entity
 
 ### Snowflake
 
+Setting up Snowflake to talk to AWS Lambda is a bit of a pain still: there is an official Snowflake tutorial, but we found easier to actually follow [this guide](https://interworks.com/blog/2020/08/14/zero-to-snowflake-setting-up-snowflake-external-functions-with-aws-lambda/): our summation lambda function in `handler.py` is actually built from that tutorial, as a simple verification the Snowflake to AWS connection works.
+
+However you decide to proceed to setup the connection, our code assume you created a DB, SCHEMA and FUNCTION according to the above tutorial, that is, resulting in something like the commands:
+
+```CREATE OR REPLACE database external_functions;
+CREATE OR REPLACE schema external_functions.lambda;
+CREATE OR REPLACE external function external_functions.lambda.resolution(x string, y string) ...```
+
+If you end up placing your external function somewhere else, _just make sure to replace the relevant calls in the dbt files_ to make sure your SQL and your setup are consistent.
+
 
 ## Run entity resolution in dbt
--- src/serverless/handler.py
+++ src/serverless/handler.py
@@ -2,14 +2,28 @@ import json
 import re
 
 
+def wrap_response(status_code: int, response):
+    return {
+        'statusCode': status_code,
+        'headers': {
+            # this makes the function callable across domains!
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json' 
+        },
+        'body' : json.dumps({"data": response})
+        }
+
+
 def summation(event, context):
     """
     This is a test function with a trivial Python logic just to check the integration
     with Snowflake.
 
     """
+    # debug
+    print(event)
     # variables we are going to return to the client
-    statusCode = 200
+    status_code = 200
     response = ''
     # try/except is used for error handling
     try:
@@ -22,13 +36,9 @@ def summation(event, context):
         # the first entry is the row index and we echo it back
         response = [[row[0], row[1] + row[2]] for row in rows]
     except Exception as err:
-        statusCode = 400
+        status_code = 400
         response = str(err)
     # print in cloudwatch for debug!
     print(response)
     # return the response to the client
-    return {
-        'statusCode': statusCode,
-        'headers': { 'Content-Type': 'application/json' },
-        'body' : json.dumps({"data": response})
-        }
+    return wrap_response(status_code, response)
